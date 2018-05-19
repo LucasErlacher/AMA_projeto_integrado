e5b213fe74572990ff10cdc111a689d824f16449
tarcisiobruni <tarcisiobruni@hotmail.com>
2018-05-19 11:55:31 -0300

    Update TestePacienteDAO.java

--- AMA/src/br/com/ama/testes/TestePacienteDAO.java
+++ AMA/src/br/com/ama/testes/TestePacienteDAO.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import br.com.ama.dao.PacienteDAO;
 import br.com.ama.modelo.Paciente;
+/*Alteração feita para teste de integracao com GitHub*/
 
 public class TestePacienteDAO {
 	public static void main(String[] args) throws SQLException {
