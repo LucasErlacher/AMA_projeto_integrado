90052b4d58a3f70a4d3974baecb889c935f3aa78
Lucas Erlacher <lucaserlacher@hotmail.com>
2018-05-26 15:51:38 -0300

    Teste

--- AMA/src/br/com/ama/modelo/Centro.java
+++
@@ -1,8 +0,0 @@
-package br.com.ama.modelo;
-
-public class Centro {
-	private int id;
-	private String nome;
-	private String cnpj;
-	
-}
--- projetoama/src/main/java/br/com/projetoama/cdp/LocalAtendimento.java
+++
@@ -1,8 +0,0 @@
-package br.com.projetoama.cdp;
-
-public class LocalAtendimento {
-	private int id;
-	private String nome;
-	private String cnpj;
-        private Endereco endereco;
-}
