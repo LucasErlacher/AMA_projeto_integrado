148f49b2989ce089581a873745e86bf063a8ec53
tarcisiobruni <tarcisiobruni@hotmail.com>
2018-05-30 21:34:38 -0300

    Arquivo de Configuracao do Travis

--- src/main/java/br/com/ama/cci/DefaultController.java
+++ src/main/java/br/com/ama/cci/DefaultController.java
@@ -1,18 +1,18 @@
-package br.com.ama.cci;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
- 
-
-@Controller
-public class DefaultController {
-    
-   @RequestMapping(value = "/")
-   public String index(ModelMap map) {
-       map.put("msg", "Hello Spring 4 Web MVC!");       
-       return "index";
-   }
-    
-}
+//package br.com.ama.cci;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.ModelMap;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+// 
+//
+//@Controller
+//public class DefaultController {
+//    
+//   @RequestMapping(value = "/")
+//   public String index(ModelMap map) {
+//       map.put("msg", "Hello Spring 4 Web MVC!");       
+//       return "index";
+//   }
+//    
+//}
