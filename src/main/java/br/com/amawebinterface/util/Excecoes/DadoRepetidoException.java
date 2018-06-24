322f8a78a0b023d593ff28e9d4d3173af6014a49
tarcisiobruni <tarcisiobruni@hotmail.com>
2018-06-24 15:05:38 -0300

    Insercao do Cadastro de AgenteSaude + melhorias

---
+++ src/main/java/br/com/amawebinterface/util/Excecoes/DadoRepetidoException.java
@@ -0,0 +1,8 @@
+package br.com.amawebinterface.util.Excecoes;
+
+
+public class DadoRepetidoException extends RuntimeException {  
+    public DadoRepetidoException(String message) {
+        super(message);
+    }
+}
